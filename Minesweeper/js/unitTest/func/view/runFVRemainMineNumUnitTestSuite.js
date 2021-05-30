a8c5b18ecc5130f2093ee90d150a53e36fbe7874
Double-X <lkllun@yahoo.com.hk>
2021-05-30 12:31:56 +0800

    Add files via upload

--- Minesweeper/js/unitTest/func/view/runFVRemainMineNumUnitTestSuite.js
+++ Minesweeper/js/unitTest/func/view/runFVRemainMineNumUnitTestSuite.js
@@ -4,7 +4,7 @@
  * @author DoubleX
  * @param {Function} $ - The component to be unit tested
  * @since v1.0
- * @version v1.0
+ * @version v1.1
  */
 DoubleX.PROJ.MINESWEEPER.TEST.UNIT.RunFVRemainMineNumUnitTestSuite = 
         function($) {
@@ -24,7 +24,7 @@ DoubleX.PROJ.MINESWEEPER.TEST.UNIT.RunFVRemainMineNumUnitTestSuite =
      * @author DoubleX
      * @param {Number} mineNum - The number of mines to be shown
      * @since v1.0
-     * @version v1.0
+     * @version v1.1
      */
     $$$._formattedMineNum = function(mineNum) {
         // Ensures the output's the expected one with respect to the input
@@ -42,64 +42,44 @@ DoubleX.PROJ.MINESWEEPER.TEST.UNIT.RunFVRemainMineNumUnitTestSuite =
         console.info("mineNum: " + mineNum);
         if (unitTestFormattedMineNum1 === "999") {
             console.info("Passed the 1st test!");
-        } else {
-            console.info("Failed the 1st test! Actual value: " + 
-                    unitTestFormattedMineNum1);
-        }
+        } else console.info("Failed the 1st test! Actual value: " + 
+                unitTestFormattedMineNum1);
         if (unitTestFormattedMineNum2 === "100") {
             console.info("Passed the 2nd test!");
-        } else {
-            console.info("Failed the 2nd test! Actual value: " + 
-                    unitTestFormattedMineNum2);
-        }
+        } else console.info("Failed the 2nd test! Actual value: " + 
+                unitTestFormattedMineNum2);
         if (unitTestFormattedMineNum3 === "099") {
             console.info("Passed the 3rd test!");
-        } else {
-            console.info("Failed the 3rd test! Actual value: " + 
-                    unitTestFormattedMineNum3);
-        }
+        } else console.info("Failed the 3rd test! Actual value: " + 
+                unitTestFormattedMineNum3);
         if (unitTestFormattedMineNum4 === "010") {
             console.info("Passed the 4th test!");
-        } else {
-            console.info("Failed the 4th test! Actual value: " + 
-                    unitTestFormattedMineNum4);
-        }
+        } else console.info("Failed the 4th test! Actual value: " + 
+                unitTestFormattedMineNum4);
         if (unitTestFormattedMineNum5 === "009") {
             console.info("Passed the 5th test!");
-        } else {
-            console.info("Failed the 5th test! Actual value: " + 
-                    unitTestFormattedMineNum5);
-        }
+        } else console.info("Failed the 5th test! Actual value: " + 
+                unitTestFormattedMineNum5);
         if (unitTestFormattedMineNum6 === "000") {
             console.info("Passed the 6th test!");
-        } else {
-            console.info("Failed the 6th test! Actual value: " + 
-                    unitTestFormattedMineNum6);
-        }
+        } else console.info("Failed the 6th test! Actual value: " + 
+                unitTestFormattedMineNum6);
         if (unitTestFormattedMineNum7 === "-01") {
             console.info("Passed the 7th test!");
-        } else {
-            console.info("Failed the 7th test! Actual value: " + 
-                    unitTestFormattedMineNum7);
-        }
+        } else console.info("Failed the 7th test! Actual value: " + 
+                unitTestFormattedMineNum7);
         if (unitTestFormattedMineNum8 === "-09") {
             console.info("Passed the 8th test!");
-        } else {
-            console.info("Failed the 8th test! Actual value: " + 
-                    unitTestFormattedMineNum8);
-        }
+        } else console.info("Failed the 8th test! Actual value: " + 
+                unitTestFormattedMineNum8);
         if (unitTestFormattedMineNum9 === "-10") {
             console.info("Passed the 9th test!");
-        } else {
-            console.info("Failed the 9th test! Actual value: " + 
-                    unitTestFormattedMineNum9);
-        }
+        } else console.info("Failed the 9th test! Actual value: " + 
+                unitTestFormattedMineNum9);
         if (unitTestFormattedMineNum10 === "-99") {
             console.info("Passed the 10th test!");
-        } else {
-            console.info("Failed the 10th test! Actual value: " + 
-                    unitTestFormattedMineNum10);
-        }
+        } else console.info("Failed the 10th test! Actual value: " + 
+                unitTestFormattedMineNum10);
         //
     }; // $$$._formattedMineNum
 
